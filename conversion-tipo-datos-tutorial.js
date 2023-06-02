a3255001f06c5e6c7137e6e77ffcb434d8a5bf57
Sergie Code <68135000+sergiecode@users.noreply.github.com>
2023-06-02 14:04:09 -0300

    Update conversion-tipo-datos-tutorial.js

--- conversion-tipo-datos-tutorial.js
+++ conversion-tipo-datos-tutorial.js
@@ -5,14 +5,12 @@ let str = String(number);
 console.log(typeof str); // "string"
 console.log(str); // "123"
 
-
 // Ejemplo de conversión de string a number
 let str = "456";
 let number = Number(str);
 
 console.log(typeof number); // "number"
 console.log(number); // 456
-
 
 // Ejemplo de conversión de boolean a string
 let bool = true;
@@ -21,14 +19,12 @@ let str = String(bool);
 console.log(typeof str); // "string"
 console.log(str); // "true"
 
-
 // Ejemplo de conversión de boolean a number
 let bool = true;
 let number = Number(bool);
 
 console.log(typeof number); // "number"
 console.log(number); // 1
-
 
 // Ejemplo de conversión de string a boolean
 let str = "true";
@@ -37,7 +33,6 @@ let bool = Boolean(str);
 console.log(typeof bool); // "boolean"
 console.log(bool); // true
 
-
 // Ejemplo de conversión de number a boolean
 let number = 0;
 let bool = Boolean(number);
@@ -45,14 +40,12 @@ let bool = Boolean(number);
 console.log(typeof bool); // "boolean"
 console.log(bool); // false
 
-
 // Ejemplo de conversión de string a array
 let str = "Hola";
 let array = Array.from(str);
 
 console.log(Array.isArray(array)); // true
 console.log(array); // ["H", "o", "l", "a"]
-
 
 // Ejemplo de conversión de array a string
 let array = ["H", "o", "l", "a"];
